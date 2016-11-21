30b38a3554e9a27ad609056fcf527cfec8357057
Erin Bond <erin@erinebond.com>
2016-11-21 13:36:10 -0800

    Each box plays the correct note on a click

--- noise.js
+++ noise.js
@@ -1,3 +1,40 @@
 $(document).ready( function() {
-  // your code here
+
+
+  $('.c').click(function(event) {
+    var sound = document.getElementById("cAudio");
+    sound.play();
+  });
+
+  $('.d').click(function(event) {
+    var sound = document.getElementById("dAudio");
+    sound.play();
+  });
+
+  $('.e').click(function(event) {
+    var sound = document.getElementById("eAudio");
+    sound.play();
+  });
+
+  $('.f').click(function(event) {
+    var sound = document.getElementById("fAudio");
+    sound.play();
+  });
+
+  $('.g').click(function(event) {
+    var sound = document.getElementById("gAudio");
+    sound.play();
+  });
+
+  $('.a').click(function(event) {
+    var sound = document.getElementById("aAudio");
+    sound.play();
+  });
+
+  $('.b').click(function(event) {
+    var sound = document.getElementById("bAudio");
+    sound.play();
+  });
+
+
 });
