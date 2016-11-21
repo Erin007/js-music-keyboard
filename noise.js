ac93138ad494a84a3fe874b5a99197dc69a611eb
Erin Bond <erin@erinebond.com>
2016-11-21 15:02:14 -0800

    Loop to make the functions that handle click events

--- noise.js
+++ noise.js
@@ -1,108 +1,106 @@
 $(document).ready( function() {
 
-  $('.c').click(function(event) {
-    var sound = document.getElementById("cAudio");
-    sound.play();
-  });
+  var assignNotes = function(){
+    var letters = ["a","b","c","d","e","f","g"];
 
-  $('.d').click(function(event) {
-    var sound = document.getElementById("dAudio");
-    sound.play();
-  });
+    for (var i = 0; i < letters.length; i++) {
 
-  $('.e').click(function(event) {
-    var sound = document.getElementById("eAudio");
-    sound.play();
-  });
-
-  $('.f').click(function(event) {
-    var sound = document.getElementById("fAudio");
-    sound.play();
-  });
-
-  $('.g').click(function(event) {
-    var sound = document.getElementById("gAudio");
-    sound.play();
-  });
-
-  $('.a').click(function(event) {
-    var sound = document.getElementById("aAudio");
-    sound.play();
-  });
-
-  $('.b').click(function(event) {
-    var sound = document.getElementById("bAudio");
-    sound.play();
-  });
-
-  $('body').keydown(function(event){
-    if (event.key == "a"){
-      var sound = document.getElementById("aAudio");
-      sound.play();
+      var clickSetter = function(){
+        var counter = i;
+        $('.'+letters[counter]).click(function(event){
+        var sound = document.getElementById(letters[counter] + "Audio");
+        sound.play();
+        });
+      };
+      clickSetter();
     }
-  });
-
-  $('body').keydown(function(event){
-    if (event.key == "b"){
-      var sound = document.getElementById("bAudio");
-      sound.play();
-    }
-  });
-
-  $('body').keydown(function(event){
-    if (event.key == "c"){
-      var sound = document.getElementById("cAudio");
-      sound.play();
-    }
-  });
-
-  $('body').keydown(function(event){
-    if (event.key == "d"){
-      var sound = document.getElementById("dAudio");
-      sound.play();
-    }
-  });
-
-  $('body').keydown(function(event){
-    if (event.key == "e"){
-      var sound = document.getElementById("eAudio");
-      sound.play();
-    }
-  });
-
-  $('body').keydown(function(event){
-    if (event.key == "f"){
-      var sound = document.getElementById("fAudio");
-      sound.play();
-    }
-  });
-
-  $('body').keydown(function(event){
-    if (event.key == "g"){
-      var sound = document.getElementById("gAudio");
-      sound.play();
-    }
-  });
+  }
+  assignNotes();
 });
 
-
-
-
-
-
-  // DESICATON!//
-  // var assignNotes = function(){
-  //   var letters = ["a","b","c","d","e","f","g"];
-  //
-  //   for (var i = 0; i < letters.length; i++) {
-  //     console.log('.'+letters[i]);
-  //     console.log(letters[i] + "Audio")
-  //
-  //     $('.'+letters[i]).click(function(event){
-  //       console.log("hi");
-  //       var sound = document.getElementById(letters[i] + "Audio");
-  //       // sound.play();
-  //     });
-  //   }
-  // }
-  // assignNotes();
+//
+//   $('.c').click(function(event) {
+//     var sound = document.getElementById("cAudio");
+//     sound.play();
+//   });
+//
+//   $('.d').click(function(event) {
+//     var sound = document.getElementById("dAudio");
+//     sound.play();
+//   });
+//
+//   $('.e').click(function(event) {
+//     var sound = document.getElementById("eAudio");
+//     sound.play();
+//   });
+//
+//   $('.f').click(function(event) {
+//     var sound = document.getElementById("fAudio");
+//     sound.play();
+//   });
+//
+//   $('.g').click(function(event) {
+//     var sound = document.getElementById("gAudio");
+//     sound.play();
+//   });
+//
+//   $('.a').click(function(event) {
+//     var sound = document.getElementById("aAudio");
+//     sound.play();
+//   });
+//
+//   $('.b').click(function(event) {
+//     var sound = document.getElementById("bAudio");
+//     sound.play();
+//   });
+//
+//   $('body').keydown(function(event){
+//     if (event.key == "a"){
+//       var sound = document.getElementById("aAudio");
+//       sound.play();
+//     }
+//   });
+//
+//   $('body').keydown(function(event){
+//     if (event.key == "b"){
+//       var sound = document.getElementById("bAudio");
+//       sound.play();
+//     }
+//   });
+//
+//   $('body').keydown(function(event){
+//     if (event.key == "c"){
+//       var sound = document.getElementById("cAudio");
+//       sound.play();
+//     }
+//   });
+//
+//   $('body').keydown(function(event){
+//     if (event.key == "d"){
+//       var sound = document.getElementById("dAudio");
+//       sound.play();
+//     }
+//   });
+//
+//   $('body').keydown(function(event){
+//     if (event.key == "e"){
+//       var sound = document.getElementById("eAudio");
+//       sound.play();
+//     }
+//   });
+//
+//   $('body').keydown(function(event){
+//     if (event.key == "f"){
+//       var sound = document.getElementById("fAudio");
+//       sound.play();
+//     }
+//   });
+//
+//   $('body').keydown(function(event){
+//     if (event.key == "g"){
+//       var sound = document.getElementById("gAudio");
+//       sound.play();
+//     }
+//   });
+// });
+//
