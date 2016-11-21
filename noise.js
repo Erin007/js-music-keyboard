efdc463689d188ee193ec598a1db3e4a5f8881b7
Erin Bond <erin@erinebond.com>
2016-11-21 15:12:12 -0800

    Loop to handle keystorke events

--- noise.js
+++ noise.js
@@ -10,97 +10,21 @@ $(document).ready( function() {
         $('.'+letters[counter]).click(function(event){
         var sound = document.getElementById(letters[counter] + "Audio");
         sound.play();
-        });
-      };
+        });//.click
+      };//clickSetter
       clickSetter();
-    }
-  }
-  assignNotes();
-});
 
-//
-//   $('.c').click(function(event) {
-//     var sound = document.getElementById("cAudio");
-//     sound.play();
-//   });
-//
-//   $('.d').click(function(event) {
-//     var sound = document.getElementById("dAudio");
-//     sound.play();
-//   });
-//
-//   $('.e').click(function(event) {
-//     var sound = document.getElementById("eAudio");
-//     sound.play();
-//   });
-//
-//   $('.f').click(function(event) {
-//     var sound = document.getElementById("fAudio");
-//     sound.play();
-//   });
-//
-//   $('.g').click(function(event) {
-//     var sound = document.getElementById("gAudio");
-//     sound.play();
-//   });
-//
-//   $('.a').click(function(event) {
-//     var sound = document.getElementById("aAudio");
-//     sound.play();
-//   });
-//
-//   $('.b').click(function(event) {
-//     var sound = document.getElementById("bAudio");
-//     sound.play();
-//   });
-//
-//   $('body').keydown(function(event){
-//     if (event.key == "a"){
-//       var sound = document.getElementById("aAudio");
-//       sound.play();
-//     }
-//   });
-//
-//   $('body').keydown(function(event){
-//     if (event.key == "b"){
-//       var sound = document.getElementById("bAudio");
-//       sound.play();
-//     }
-//   });
-//
-//   $('body').keydown(function(event){
-//     if (event.key == "c"){
-//       var sound = document.getElementById("cAudio");
-//       sound.play();
-//     }
-//   });
-//
-//   $('body').keydown(function(event){
-//     if (event.key == "d"){
-//       var sound = document.getElementById("dAudio");
-//       sound.play();
-//     }
-//   });
-//
-//   $('body').keydown(function(event){
-//     if (event.key == "e"){
-//       var sound = document.getElementById("eAudio");
-//       sound.play();
-//     }
-//   });
-//
-//   $('body').keydown(function(event){
-//     if (event.key == "f"){
-//       var sound = document.getElementById("fAudio");
-//       sound.play();
-//     }
-//   });
-//
-//   $('body').keydown(function(event){
-//     if (event.key == "g"){
-//       var sound = document.getElementById("gAudio");
-//       sound.play();
-//     }
-//   });
-// });
-//
+      var keystrokeSetter = function() {
+        var counter = i;
+        $('body').keydown(function(event){
+            if (event.key == letters[counter]){
+              var sound = document.getElementById(letters[counter] +"Audio");
+              sound.play();
+            }//conditional
+          });//.keydown
+      }//keystrokeSetter
+      keystrokeSetter();
+    }//assignNotes
+  }//loop
+  assignNotes();
+});//whole document
